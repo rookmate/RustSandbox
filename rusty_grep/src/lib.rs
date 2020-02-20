9bc7e65aa463231b8b37de1d8ee5aacee513838f
rookmate <14072042+rookmate@users.noreply.github.com>
2020-02-20 17:44:42 +0000

    [GREP] Search case insensitive function. Unittest on it.

--- rusty_grep/src/lib.rs
+++ rusty_grep/src/lib.rs
@@ -1,10 +1,12 @@
 use std::fs;
+use std::env;
 use std::error::Error;
 
 // Basic conf struct
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 // Init a new conf
@@ -16,8 +18,10 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        // Checks value of env var from the command line
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -25,7 +29,13 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents){
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -45,6 +55,21 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+// Search case insensitive
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,5 +86,20 @@ FeelsGoodMan";
             vec!["Chuck Testa"],
             search(query, contents)
         );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "Huc";
+        let contents = "
+Chuck Testa
+Rust unit-tests
+FeelsGoodMan
+Huckleberry Trust";
+
+        assert_eq!(
+            vec!["Chuck Testa", "Huckleberry Trust"],
+            search_case_insensitive(query, contents)
+        )
     }
 }
