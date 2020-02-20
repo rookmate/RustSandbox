fed53672bcb7d40be0276671e9876fc926897239
rookmate <14072042+rookmate@users.noreply.github.com>
2020-02-20 14:39:32 +0000

    [GREP] Move function as method

--- rusty_grep/src/main.rs
+++ rusty_grep/src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     println!("Filename: {}", config.filename);
     println!("Finding: {}", config.query);
 
@@ -22,9 +22,15 @@ struct Config {
 }
 
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
 
-    Config { query, filename }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
