5ddedd1394c274d28def1cd55332eab0c063b074
rookmate <14072042+rookmate@users.noreply.github.com>
2020-02-20 13:17:32 +0000

    [GREP] Add basic structure

--- rusty_grep/src/main.rs
+++ rusty_grep/src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+    println!("Filename: {}", config.filename);
+    println!("Finding: {}", config.query);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Could not read file");
+
+    println!("File content:\n{}", contents);
+}
+
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
