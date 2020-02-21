b8bced4a04c2b5d737e8c6772ab77dee54dbb00f
rookmate <14072042+rookmate@users.noreply.github.com>
2020-02-21 12:22:09 +0000

    [GREP] Use filter to search file.

--- rusty_grep/src/lib.rs
+++ rusty_grep/src/lib.rs
@@ -52,15 +52,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 // Search file for string
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 // Search case insensitive
