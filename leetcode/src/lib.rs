fe99f7c11936c3baf773991ac100a145699f285b
rookmate <14072042+rookmate@users.noreply.github.com>
2020-02-24 13:21:19 +0000

    [LEETCODE] Add Reverse Integer comments

--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -40,10 +40,15 @@ pub fn zigzag_conversion(s: String, num_rows: i32) -> String {
 // reverse(-123);
 // reverse(1534236469);
 pub fn reverse_integer(x: i32) -> i32 {
+    // if number is negative, should change it back at the end
     let flag = x.is_negative();
 
+    // make it positive
+    // convert to char
+    // revert order
     let s = x.abs().to_string().chars().rev().collect::<String>();
 
+    // convert to int and add sign if needed
     let i = match s.parse::<i32>() {
         Err(_e) => return 0,
         Ok(i) => i,
